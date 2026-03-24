22ad654cd87390bb1dcfac075daf60b43ba03449
Manos Damianakis <manosdam57@gmail.com>
2026-03-24 11:37:44 +0100

    Update main.tf

--- research-and-developement/terraform_test/main.tf
+++ research-and-developement/terraform_test/main.tf
@@ -1,4 +1,3 @@
-# 1. Ορίζουμε τον Provider για τοπικά αρχεία
 terraform {
   required_providers {
     local = {
@@ -8,14 +7,12 @@ terraform {
   }
 }
 
-# 2. Λέμε στην Terraform να φτιάξει ένα αρχείο κειμένου
-resource "local_file" "capco_note" {
-  filename = "${path.module}/capco_onboarding.txt"
+resource "local_file" "note" {
+  filename = "${path.module}/onboarding.txt"
   content  = "Welcome Manos to the AI Engineering Team! Project: FastAPI + React + Angular"
 }
 
-# 3. Λέμε στην Terraform να φτιάξει έναν δεύτερο αρχείο με το όνομα του πρώτου
 resource "local_file" "backup_note" {
   filename = "${path.module}/backup.txt"
-  content  = "This is a backup of: ${local_file.capco_note.content}"
+  content  = "This is a backup of: ${local_file.note.content}"
 }
